e4cce0e5a91c6b23d74287d56b380b1424373d97
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-02-25 19:11:14 +0000

    Update Mad_Libs.java

--- Java/Basic/Mad_Libs.java
+++ Java/Basic/Mad_Libs.java
@@ -23,7 +23,7 @@ public class MadLibs {
       int number = 2552;
       String place1 = "Reach";
 
-      //The template for the story
+      //The story
       String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
 
       System.out.println(story);
