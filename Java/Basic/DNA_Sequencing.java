3e3dedaf00aa0059197704edac9e3b7338014464
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-02-29 15:00:05 +0000

    Update DNA_Sequencing.java

--- Java/Basic/DNA_Sequencing.java
+++ Java/Basic/DNA_Sequencing.java
@@ -4,3 +4,39 @@
   Date: 06/06/2022
   */
 
+public class DNA {
+  
+  public static void main(String[] args) {
+             
+    //  -. .-.   .-. .-.   .
+    //    \   \ /   \   \ / 
+    //   / \   \   / \   \  
+    //  ~   `-~ `-`   `-~ `-
+    String dna1 = "ATGCGATACGCTTGA";
+    String dna2 = "ATGCGATACGTGA";
+    String dna3 = "ATTAATATGTACTGA";
+
+    //String dna = dna1;
+    //String dna = dna2;
+    String dna = dna3;
+    
+    int length = dna.length();
+    System.out.println("Length: " + length);
+
+    int beginning = dna.indexOf("ATG");
+    System.out.println("Start: " + beginning);
+
+    int end = dna.indexOf("TGA");
+    System.out.println("Start: " + end);
+
+    if (beginning != -1 && 
+        end != -1 && 
+        (end - beginning) % 3 == 0){
+      
+    String protein = dna.substring(beginning, end+3); 
+    System.out.println("Protein: " + protein);    
+    } else {
+    System.out.println("No protein.");
+    }
+  }
+}
