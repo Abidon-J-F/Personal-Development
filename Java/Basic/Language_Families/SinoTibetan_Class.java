85fd75ae5110b52842718d6e2692ee31d218c541
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-02-29 21:55:17 +0000

    Create SinoTibetan_Class.java

---
+++ Java/Basic/Language_Families/SinoTibetan_Class.java
@@ -0,0 +1,14 @@
+ /*
+  This SinoTibetan Class is part of the Language Families program.
+  Author: Abidon Jude Fernandes
+  Date: 03/07/2022
+  */
+
+class SinoTibetan extends Language {
+  SinoTibetan(String languageName, int speakers) {
+    super(languageName, speakers, "Asia", "subject-object-verb");
+    if (languageName.contains("Chinese")) {
+      this.wordOrder = "subject-verb-object";
+    }
+  }
+}
