d84deddce0bf87eafb6a251bc6cb923b71bc11e3
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-03-02 13:11:53 +0000

    Update Tip_Calculator.py

--- Python/Basic/Tip_Calculator.py
+++ Python/Basic/Tip_Calculator.py
@@ -1,10 +1,14 @@
-
+  """
+  This is a Tip Calculator made in Python2.
+  Author: Abidon Jude Fernandes
+  Date: 02/02/2024
+  """
 
 meal = 44.50
 tax = 0.0675
 tip = 0.15
 
-wallet = 60
+wallet = 75
 
 meal = meal + meal * tax
 total = meal + meal * tip
