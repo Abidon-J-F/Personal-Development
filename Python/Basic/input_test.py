07973140599606c4f9c8a0871ab1da9b8bf53398
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-03-03 21:25:54 +0000

    Add files via upload

---
+++ Python/Basic/input_test.py
@@ -0,0 +1,10 @@
+""" 
+This is an Input Test program made in Python3.
+Author: Abidon Jude Fernandes
+Date: 01/2022 - 01/2022
+"""
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+print("Your full name is", firstname, lastname)
---
+++ Python/Basic/test.py
@@ -0,0 +1,7 @@
+""" 
+This is an Test program made in Python3.
+Author: Abidon Jude Fernandes
+Date: 01/2022 - 01/2022
+"""
+
+print("Subscribe Now")
