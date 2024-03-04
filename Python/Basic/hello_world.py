bca3c53e15275db00c37498d685ac9a5f77c709f
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-03-04 17:24:23 +0000

    Delete Python/Basic/hello_world.py

--- Python/Basic/hello_world.py
+++
@@ -1,9 +0,0 @@
-""" 
-This is an Hello World program made in Python3.
-Author: Abidon Jude Fernandes
-Date: 03/2022 - 04/2022
-"""
-
-##Indenting Unnecessarily
-#message = "Hello Python world!"
-#    print(message)
