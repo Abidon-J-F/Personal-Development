ae350ee714e65cf031bd00583f64b771f9b24a62
Abidon J Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-03-04 17:22:16 +0000

    Delete Python/Basic/comment.py

--- Python/Basic/comment.py
+++
@@ -1,9 +0,0 @@
-""" 
-This is an Comment program made in Python3.
-Author: Abidon Jude Fernandes
-Date: 01/2022 - 02/2022
-"""
-
-# (hash mark) = Comment
-# Say hello to everyone.
-print("Hello Python people!")
