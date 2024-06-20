99297a27e4bb2f9ce2e2eddc51917c535392e157
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:54:06 +0100

    Update IntUserInputRetriever.java

--- Java/Intermediate/Let's Get Takeout!/IntUserInputRetriever.java
+++ Java/Intermediate/Let's Get Takeout!/IntUserInputRetriever.java
@@ -1,3 +1,9 @@
+/*
+This is part of Let's Get Takeout! program.
+Author: Abidon Jude Fernandes
+Date: 11/2023-12/2023
+*/
+
 public interface IntUserInputRetriever<T> {
 
     T produceOutputOnIntUserInput(int selection) throws IllegalArgumentException;
