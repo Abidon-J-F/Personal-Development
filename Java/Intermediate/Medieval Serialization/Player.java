bafefc11f0b07537b4aa2b9663cf2e12fe66aecd
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:50:27 +0100

    Update Player.java

--- Java/Intermediate/Medieval Serialization/Player.java
+++ Java/Intermediate/Medieval Serialization/Player.java
@@ -1,5 +1,5 @@
 /*
-This is 2 of 2 main parts of the Medieval Serialization program.
+This is 2 of 2 Main Classes of the Medieval Serialization program.
 Author: Abidon Jude Fernandes
 Date: 09/2023-10/2023
 */
