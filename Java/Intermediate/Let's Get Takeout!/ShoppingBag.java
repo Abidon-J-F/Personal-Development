d2165d104109faddff2b3d5c2aa153685f3878a3
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:41:58 +0100

    Create ShoppingBag.java

---
+++ Java/Intermediate/Let's Get Takeout!/ShoppingBag.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ShoppingBag <T extends PricedItem <Integer> > {
+    private Map<T,Integer> shoppingBag;
+
+    // Constructor
+    public ShoppingBag() {
+
+        this.shoppingBag = new HashMap<>();
+    }
+
+    public void addItem(T item){
+
+
+        int acumulador = 0;
+        if (!shoppingBag.containsKey(item)){
+            if (!shoppingBag.isEmpty()) {
+                shoppingBag.put(item, acumulador += 1);
+                System.out.println(acumulador);
+            } else {
+                shoppingBag.put(item, acumulador = 1);
+                System.out.println(acumulador);
+            }
+        }
+
+    }
+
+    public int getTotalPrice(){
+
+        int totalPrice = 0;
+
+        for (Map.Entry<T, Integer> eachPair : shoppingBag.entrySet() ){
+
+            totalPrice += eachPair.getKey().getPrice() * eachPair.getValue();
+        }
+        return totalPrice;
+    }
+}
