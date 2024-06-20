ddd20d0c763a4c1666815f5b8a37f15392b8df39
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:37:44 +0100

    Create Customer.java

---
+++ Java/Intermediate/Let's Get Takeout!/Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+    private String name;
+    private int money;
+
+    public Customer(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
