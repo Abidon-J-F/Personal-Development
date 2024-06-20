a25b32b0c81283c14a2175a7b2ed0375f119221e
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:39:38 +0100

    Create Food.java

---
+++ Java/Intermediate/Let's Get Takeout!/Food.java
@@ -0,0 +1,33 @@
+public class Food implements PricedItem<Integer> {
+
+    private String name;
+    private String description;
+    private int price;
+
+    public Food(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("\n %s: %s    Cost: %d\n", this.name, this.description, this.price);
+    }
+}
