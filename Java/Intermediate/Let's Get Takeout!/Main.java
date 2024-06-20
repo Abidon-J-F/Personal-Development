f78721bbd01b79ad0f55378b1975852a3d2c5daa
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-06-20 19:36:48 +0100

    Update Main.java

--- Java/Intermediate/Let's Get Takeout!/Main.java
+++ Java/Intermediate/Let's Get Takeout!/Main.java
@@ -1,1 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hi there! Please tell me your name: ");
+        String customerName = input.nextLine();
+
+        System.out.println("Please indicate how much money you have to spend, do so with int numbers :");
+
+        int money = input.nextInt();
+        if (money <= 0){
+            System.out.println("Please add more money to be able to shop.");
+        }
+
+        Customer customer = new Customer(customerName,money);
+        TakeOutSimulator takeOutSimulator = new TakeOutSimulator(customer,input);
+
+        takeOutSimulator.startTakeOutSimulator();
+    }
+}
